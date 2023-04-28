a6e356ea9d8e1815a7bfbc482d14c6e0062a4d5d
bulatovad <damira.bulatova@gmail.com>
2023-04-29 00:53:19 +0600

    solution

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,26 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+
+        ad.add(firstQueue.poll());
+        ad.add(firstQueue.poll());
+        ad.add(secondQueue.poll());
+        ad.add(secondQueue.poll());
+
+        int move = 0;
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
+            if(move%2==0) {
+                firstQueue.add(ad.removeLast());
+                ad.add(firstQueue.poll());
+                ad.add(firstQueue.poll());
+            } else {
+                secondQueue.add(ad.removeLast());
+                ad.add(secondQueue.poll());
+                ad.add(secondQueue.poll());
+            }
+            move++;
+        }
+        return ad;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,31 @@
 package com.epam.collections.queue;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Queue<Integer> dishes = new LinkedList<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i=1; i<=numberOfDishes; i++) {
+            dishes.add(i);
+        }
+
+        int dishnum = 0;
+        while (!dishes.isEmpty()){
+            Integer dish = dishes.remove();
+            dishnum++;
+
+            if(dishnum!=everyDishNumberToEat){
+                dishes.add(dish);
+            } else {
+                dishnum =0;
+                queue.add(dish);
+            }
+        }
+
+        return queue.stream().toList();
     }
 }
